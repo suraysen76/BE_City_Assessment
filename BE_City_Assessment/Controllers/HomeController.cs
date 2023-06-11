99ff343caaeb6ec32f0a726d684392c043af34f0
Suraysen Thiyagarasu <suraysen@yahoo.com>
2023-06-12 02:50:47 +0800

    first draft

--- BE_City_Assessment/Controllers/HomeController.cs
+++ BE_City_Assessment/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace BE_City_Assessment.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel  { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
